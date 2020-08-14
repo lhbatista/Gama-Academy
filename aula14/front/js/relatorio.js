d5bf8a2ac19aab74cfe0e724f91061dcb69b303f
luiis_henrique@hotmail.com <luiis_henrique@hotmail.com>
2020-08-14 15:44:50 -0300

    [Att] - Front

--- aula14/front/js/relatorio.js
+++ aula14/front/js/relatorio.js
@@ -1,31 +1,31 @@
-function validaLogin(){
+function validaLogin() {
 
     let userTxt = localStorage.getItem("userLogged");
 
-    if(!userTxt){
+    if (!userTxt) {
         window.location = "index.html";
     }
 
     let jsonUser = JSON.parse(userTxt);
 
-    document.getElementById("user").innerHTML = `${jsonUser.nome} ( ${jsonUser.racf} )` ;
+    document.getElementById("user").innerHTML = `${jsonUser.nome} ( ${jsonUser.racf} )`;
     document.getElementById("imgUser").innerHTML = `<img src ="${jsonUser.linkFoto}">`;
-    
+
     obterAgencias();
 }
 
-function logout(){
+function logout() {
     localStorage.removeItem("userLogged");
     window.location = "index.html";
 }
 
-function obterAgencias(){
+function obterAgencias() {
     fetch("http://localhost:8080/agencias")
-    .then(res => res.json())
-    .then(result => preencheAgencias(result));
+        .then(res => res.json())
+        .then(result => preencheAgencias(result));
 }
 
-function preencheAgencias(resposta){
+function preencheAgencias(resposta) {
     let agencias = '';
 
     for (let index = 0; index < resposta.length; index++) {
@@ -35,18 +35,18 @@ function preencheAgencias(resposta){
     document.getElementById("sel_agencias").innerHTML = agencias;
 }
 
-function buscar(){
+function buscar() {
     let agencia = document.getElementById("sel_agencias");
     let agenciaId = agencia[agencia.selectedIndex].value; //obtem id da agencia selecionada 
 
-    fetch("http://localhost:8080/agencia/"+agenciaId)
-    .then(res => res.json())
-    .then(result => preencheResposta(result));
+    fetch("http://localhost:8080/agencia/" + agenciaId)
+        .then(res => res.json())
+        .then(result => preencheResposta(result));
 }
 
 
 
-function preencheResposta(resposta){
+function preencheResposta(resposta) {
     let agendas = '<table class = "table"> <tr> <th>cliente</th> <th>data</th> <th>hora</th> </tr>';
 
     for (let index = 0; index < resposta.agendamentos.length; index++) {
@@ -60,12 +60,12 @@ function preencheResposta(resposta){
     document.getElementById("tableResposta").innerHTML = agendas;
 }
 
-function buscarPorData(){
+function buscarPorData() {
     let data = document.getElementById("dataAgendamento").value;
 
-    fetch("http://localhost:8080/agendamentos/data?dataagendamento="+data)
-    .then(res => res.json())
-    .then(result => preencheRespostaAgendamento(result));
+    fetch("http://localhost:8080/agendamentos/data?dataagendamento=" + data)
+        .then(res => res.json())
+        .then(result => preencheRespostaAgendamento(result));
 }
 
 function preencheRespostaAgendamento(resposta) {
@@ -84,11 +84,36 @@ function preencheRespostaAgendamento(resposta) {
     document.getElementById("tableResposta").innerHTML = agendas;
 }
 
-function nomeClient(){
+function nomeClient() {
     let cliente = document.getElementById("txtUsuario");
-    let nome = cliente.value; 
+    let nome = cliente.value;
 
-    fetch("http://localhost:8080/agendamentos/cliente/"+nome)
-    .then(res => res.json())
-    .then(result => preencheRespostaAgendamento(result));
+    fetch("http://localhost:8080/agendamentos/cliente/" + nome)
+        .then(res => res.json())
+        .then(result => preencheRespostaAgendamento(result));
+}
+
+function obterAgendamentos() {
+    fetch("http://localhost:8080/agendamentos/")
+        .then(res => res.json())
+        .then(result => preencheRespostaAgendamento(result));
+
+}
+
+function verificaCheck() {
+
+    let verifica1 = document.getElementById("check_cliente").checked;
+    let verifica2 = document.getElementById("check_data").checked;
+    let verifica3 = document.getElementById("check_ag").checked;
+
+    if (verifica1 == true) {
+        nomeClient();
+    }
+    if (verifica2 == true) {
+        buscarPorData();
+    }
+    if (verifica3 == true) {
+        buscar();
+    } else {
+         obterAgendamentos(); }
 }
